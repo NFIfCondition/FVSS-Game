40e90dee3290e3eb83adf451011252acb2f825c9
whoischris <christopherbrehm86@gmail.com>
2021-04-06 15:25:47 +0200

    Ladebild hinzugefügt

--- FVSS Game/GameScene.swift
+++ FVSS Game/GameScene.swift
@@ -9,7 +9,15 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    let loadingImage = SKSpriteNode(imageNamed: "loadingimage")
+    
     override func didMove(to view: SKView) {
+        
+        loadingImage.anchorPoint = CGPoint.zero
+        loadingImage.position = CGPoint.zero
+        loadingImage.size = self.size
+        loadingImage.zPosition = -1
+        self.addChild(loadingImage)
         
     }
 }
