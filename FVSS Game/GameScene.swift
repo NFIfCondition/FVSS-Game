472d922497d4d617d8bf611c13df8a9988782876
IfCondtion <nierlichbenjamin@gmail.com>
2021-04-10 20:23:28 +0200

    Start Ingame Scene

--- FVSS Game/GameScene.swift
+++ FVSS Game/GameScene.swift
@@ -102,14 +102,44 @@ class selectCharakter: SKScene{
 
 //INGAME SCENE
 class ingame: SKScene{
-        
+    
+    var ground = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
- 
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        createGround()
     }
     
     override func update(_ currentTime: TimeInterval) {
-        
+        moveGrounds()
     }
+    
+    func createGround(){
+        for i in 0...3 {
+            let ground = SKSpriteNode(imageNamed: "background")
+            ground.name = "Background"
+            ground.size = CGSize(width: (self.scene?.size.width)!, height: (self.scene?.size.height)!)
+            ground.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            ground.position = CGPoint(x: CGFloat(i) * ground.size.width, y: -(self.frame.size.height / 1000))
+            
+            self.addChild(ground)
+        }
+    }
+    
+    func moveGrounds(){
+        self.enumerateChildNodes(withName: "Background", using: ({
+            (node, error) in
+            
+            node.position.x -= 0.2
+            
+            if node.position.x < -((self.scene?.size.width)!){
+                node.position.x += (self.scene?.size.width)! * 3
+            }
+            
+        }))
+    }
+        
+    
     
 }
 
